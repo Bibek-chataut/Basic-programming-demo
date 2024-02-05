f3b5041ea19a012f8b3c5dc4b9bafc86dee7fd70
Bibek-chataut <bibekchataut123@gmail.com>
2024-02-05 16:14:20 +0545

    average

---
+++ practice.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+   int a,b,c,avg;
+   printf("Enter three number:\n");
+   scanf("%d%d%d",&a,&b,&c);
+   avg= (a+b+c)/3;
+   printf("The average of three numbers is:%d\n",avg);
+    return 0;
+
+}
